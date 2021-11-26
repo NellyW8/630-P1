f629914279ce138110992256ababb1276734c69c
nott1999 <55903263+nott1999@users.noreply.github.com>
2021-11-26 10:07:12 -0500

    Update dftmax.m

--- dftmax.m
+++ dftmax.m
@@ -5,6 +5,7 @@ dft_f1=zeros(84,1,'int64');
 for m=1:84
     inputdft=[signal_r2(m:m+127,1) signal_r2(m:m+127,2)];
     signal_dft1=fidft(inputdft);
+    %Need to rewirte getabs function
     dft2=getabs(signal_dft1);
     [dft_max1(m),dft_f1(m)]=max(dft2);
 end
